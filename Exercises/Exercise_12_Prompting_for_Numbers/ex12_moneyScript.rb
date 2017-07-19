f84589b45188409f94cb61b3210f8c41ce6b4772
Tom Riley <7om.riley@gmail.com>
2017-07-19 17:04:00 +0100

    Updated with new exercises.

---
+++ Exercises/Exercise_12_Prompting_for_Numbers/ex12_moneyScript.rb
@@ -0,0 +1,10 @@
+
+print "Give me some money "
+number = gets.chomp.to_i
+
+print "Actually...I feel bad, and I'm going to return some money to you...How much would you like? "
+giveback = gets.chomp.to_i
+actually = number / 10
+
+print "Well, I'm only going to give you back #{actually}...see ya sucker! *runs away*"
+Exercises
